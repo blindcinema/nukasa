d0ea6e4d999a3fa8e702d943c7d7f315f45ac3c9
blindcinema <jannemetguzvic@gmail.com>
2023-04-18 18:53:25 +0200

    initial design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,56 +1,63 @@
+import { IoMdSearch } from "react-icons/io";
+
 export function Navbar(props) {
     return (
-        <div className="Header min-w-full bg-white md:h-20 flex items-center">
-        <h1 className="site_logo m-0 py-5 text-5xl align-middle mr-10">NUKASA</h1>
+        <div className="Header min-w-full bg-white h-20 flex items-center">
+        <a className="site_logo min-w-max" href="#">
+          <div className=" ml-0 py-5  mr-10 flex items-center">
+              <img src="./svg/LG - Para.svg" className="h-20 ml-2 mr-2 scale-95" />
+              <img src="./svg/Nukasa Text.svg" className="h-10 min-h-10" />  
+          </div>  
+          </a>
+
         <div className="spacer w-3/5"></div>
+
         <div className="navbar flex items-center w-max h-full">
 
-          <ul className= "navlist text-lg inline-flex p-1 flex-row place-content-between mr-10">
+          <ul className= "navlist text-lg inline-flex p-1 flex-row place-content-between mr-10 text-[#9B4C44] font-[600]">
             
-            <li className="group pr-20 relative">Education
-              <div className="hidden group-hover:flex flex-col bg-white absolute flex-wrap divide-y divide-solid divide-black w-min">
+            <li className="group pr-20 relative">
+              <a className="hover:text-[#6A3630]" href="#">Home</a>
+            </li>
 
+            
+            <li className="group pr-20 relative"><div className="hover:text-[#6A3630]">Education</div>
+              <div className="navlist__dropdown shadow-lg hidden group-hover:flex flex-col bg-white absolute flex-wrap ">
+                <a className="mt-2 hover:text-[#6A3630]" href="#">Sound Design</a>
+                <a className="mt-2 hover:text-[#6A3630]" href="#">Arrangement</a>
+                <a className="mt-2 hover:text-[#6A3630]" href="#">Theory/Songwriting</a>
+                <a className="mt-2 hover:text-[#6A3630]" href="#">Lifestyle & Wellbeing</a>
+                <a className="mt-2 hover:text-[#6A3630]" href="#">Mixing</a>
+                <a className="mt-2 hover:text-[#6A3630]" href="#">Mastering</a>
               </div>
             </li>
             
-            <li className="group pr-20 relative">Glossary
-              <div className="hidden group-hover:flex flex-col bg-white absolute flex-wrap divide-y divide-solid divide-black w-min">
-                  <a className="" href="#">ASDF</a>
-                  <a className="" href="#">YE</a>
-                  <a className="" href="#">ALONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
+            <li className="group pr-20 relative"><div className="hover:text-[#6A3630]">Glossary</div>
+              <div className="navlist__dropdown shadow-lg hidden group-hover:flex flex-col bg-white absolute flex-wrap">
+                  <a className="mt-2 hover:text-[#6A3630]" href="#">ASDF</a>
+                  <a className="mt-2 hover:text-[#6A3630]" href="#">YE</a>
+                  <a className="mt-2 hover:text-[#6A3630]" href="#">ALONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
                 </div>
               </li>
               
-            <li className="group pr-20 relative">Lessons
-              <div className="hidden group-hover:flex flex-col bg-white absolute flex-wrap divide-y divide-solid divide-black w-min">
-                  <a class="" href="#">SWAG</a>
-                  <a class="" href="#">ASDF</a>
-                  <a class="" href="#">LONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
-                  <a class="" href="#">LONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
-                  <a class="" href="#">LONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
-                  <a class="" href="#">LONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
-                  <a class="" href="#">LONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
-                  <a class="" href="#">LONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
-
+            <li className="group pr-20 relative"><div className="hover:text-[#6A3630]">Lessons</div>
+              <div className="navlist__dropdown shadow-lg hidden group-hover:flex flex-col bg-white absolute flex-wrap">
                 </div>
             </li>
 
-            <li className="group pr-20 relative">Blogs
-              <div className="hidden group-hover:flex flex-col bg-white absolute flex-wrap divide-y divide-solid divide-black w-min">
-                  <a class="" href="#">ASDF</a>
-                  <a class="" href="#">ASDF</a>
-                  <a class="" href="#">ASDF</a>
-                  <a class="" href="#">LONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
-                  <a class="" href="#">LONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
+            <li className="group pr-20 relative"><div className="hover:text-[#6A3630]">Blogs</div>
+              <div className="navlist__dropdown shadow-lg hidden group-hover:flex flex-col bg-white absolute flex-wrap ">
+              <a className="mt-2 hover:text-[#6A3630]" href="#">ASDF</a>
+                  <a className="mt-2 hover:text-[#6A3630]" href="#">YE</a>
+                  <a className="mt-2 hover:text-[#6A3630]" href="#">ALONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
+                  <a className="mt-2 hover:text-[#6A3630]" href="#">ASDF</a>
+                  <a className="mt-2 hover:text-[#6A3630]" href="#">YE</a>
+                  <a className="mt-2 hover:text-[#6A3630]" href="#">ALONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
                 </div>
             </li>
 
-            <li className="group pr-20 relative">Search
-              <div className="hidden group-hover:flex flex-col bg-white absolute flex-wrap divide-y divide-solid divide-black w-min">
-                  <a class="" href="#">ASDF</a>
-                  <a class="" href="#">ASDF</a>
-                  <a class="" href="#">ASDF</a>
-                  <a class="" href="#">LONG ASS PIECE OF TEXT LMAO THIS IS LOOOONG</a>
+            <li className="group pr-0 relative flex items-center"><IoMdSearch size={"2rem"} className="font-extralight hover:text-[#6A3630]" />
+              <div className="navlist__dropdown shadow-lg hidden group-hover:flex flex-col bg-white absolute flex-wrap">
                 </div>
             </li>
 
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,11 +3,11 @@ import { Navbar } from "@/components/Navbar";
 export default function Dashboard(props) {
 
     return  (
-      <div className="wrapper min-h-screen bg-yellow-50" >
+      <div className="wrapper min-h-screen bg-[#F6E2C9]" >
         <Navbar></Navbar>
-        <div className="stripe bg-purple-700 h-10 w-full flex items-center text-2xl leading-10">
+        <div className="stripe bg-[#29294E] h-10 w-full flex items-center text-2xl leading-10">
           <div className="stripe__spacer w-2/12"></div>
-          <div className="text-yellow-50">TAKE IT EASY; BUT TAKE IT</div>
+          <div className="text-white mr-2" >TAKE IT EASY; </div> <div className="text-[#F6E2C9]"> BUT TAKE IT</div>
         </div>
 
       </div>
