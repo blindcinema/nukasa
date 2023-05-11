d1eae5f41759aebc2e5de67b99a39cbe444a77cf
blindcinema <jannemetguzvic@gmail.com>
2023-05-11 15:23:29 +0200

    apostrophe lol

--- src/components/ColorPosts.jsx
+++ src/components/ColorPosts.jsx
@@ -10,7 +10,7 @@ export default function ColorPosts({ post }) {
         <div className="grid grid-cols-3 mt-16 ">
             <div className="bg-[#C8D3AD] text-[--nukasa_red] w-full h-[32rem] aspect-square flex flex-col flex-wrap">
                 <h3 className={`${inter.className} mt-12 ml-8 text-3xl font-thin`}>Personal Blog</h3>
-                <div className={`font-[Oswald] ml-12 mt-12 text-4xl leading-relaxed h-32 pr-8`}>Listen Mane, I've been working Crazy all day and night to make This shit work and I'm losing it High key</div>
+                <div className={`font-[Oswald] ml-12 mt-12 text-4xl leading-relaxed h-32 pr-8`}>Listen Mane, I&apos;ve been working Crazy all day and night to make This shit work and I&apos;m losing it High key</div>
                 <div className="uppercase ml-12 mt-32 font-[Oswald]">by Simiux</div>
                 <MoreButton className="ml-12 text-center flex mt-8 w-min text-3xl"></MoreButton>
             </div>
@@ -22,7 +22,7 @@ export default function ColorPosts({ post }) {
             </div>
             <div className="bg-[#EFCC5F] text-[--nukasa_red] w-full h-[32rem] aspect-square " >
                 <h3 className={`${inter.className} mt-12 ml-8 text-3xl font-thin`}>Personal Blog</h3>
-                <div className={`font-[Oswald] ml-12 mt-12 text-4xl leading-relaxed h-32 pr-8`}>There's a lot of text here, oh boy!! I hope it doesn't overflow and fuck up the rest of the div or the page!!!</div>
+                <div className={`font-[Oswald] ml-12 mt-12 text-4xl leading-relaxed h-32 pr-8`}>There&apos;s a lot of text here, oh boy!! I hope it doesn&apos;t overflow and fuck up the rest of the div or the page!!!</div>
                 <div className="uppercase ml-12 mt-32 font-[Oswald]">by Simiux</div>
                 <MoreButton className="ml-12 text-center flex mt-8 w-min text-3xl"></MoreButton>
             </div>
